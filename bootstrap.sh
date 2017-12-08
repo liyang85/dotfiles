7e47684f2b5e673a935a325f2925eb5b79335d45
Li Yang <li.will.yang@gmail.com>
2017-12-08 17:03:47 +0800

    copy different files to macOS and Linux separately

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
+function doItMac() {
 	rsync --exclude ".git/" 		\
 		--exclude ".DS_Store" 		\
 		--exclude ".osx" 		\
@@ -15,13 +15,45 @@ function doIt() {
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+function doItLinux() {
+	rsync --exclude ".git/" 		\
+		--exclude ".DS_Store" 		\
+		--exclude ".osx" 		\
+		--exclude ".macos"		\
+		--exclude "brew.sh"		\
+		--exclude "bin/subl"		\
+		--exclude "init/"		\
+		--exclude "bootstrap.sh" 	\
+		--exclude "README.md" 		\
+		--exclude "LICENSE-MIT.txt" 	\
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+os=`uname`
+
+if [[ $os == Darwin ]]; then
+	if [ "$1" == "--force" -o "$1" == "-f" ]; then
+		doItMac;
+	else
+		read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+		echo "";
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			doItMac;
+		fi;
 	fi;
-fi;
-unset doIt;
+	unset doItMac;
+fi
+
+if [[ $os == Linux ]]; then
+	if [ "$1" == "--force" -o "$1" == "-f" ]; then
+		doItLinux;
+	else
+		read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+		echo "";
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			doItLinux;
+		fi;
+	fi;
+	unset doItLinux;
+fi
