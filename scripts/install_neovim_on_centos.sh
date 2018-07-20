2e00e9fbe5408c78744f4369daafcd08164abf6c
Li Yang <li.will.yang@gmail.com>
2018-07-20 13:27:16 +0800

    Fix some minor bugs; nicer output

--- scripts/install_neovim_on_centos.sh
+++ scripts/install_neovim_on_centos.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 #===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
 # Filename:		install_neovim_on_centos.sh
@@ -8,48 +8,62 @@
 # Website:		https://liyang85.com
 #===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== 
 
+if [ ! -x "$(command -v wget)" ]; then
+	yum install -y wget
+fi
+
+echo -e "\nDownloading neovim from official GitHub repo ...\n"
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
 	-qO nvim &> /dev/null
 
 chmod +x nvim
 chown root:root nvim
-
 mv nvim /usr/local/bin/
-#ln -s nvim /usr/local/bin/nvi
 
 nvimDir="${HOME}/.config/nvim"
 nvimCfg="${nvimDir}/init.vim"
 if [ ! -d "${nvimDir}" ]; then
-	mkdir -p "${nvimDir}"
+	mkdir -pv "${nvimDir}"
 fi
+echo
+
 if [ ! -e "${nvimCfg}" ]; then 
 	wget https://raw.githubusercontent.com/liyang85/dotfiles/master/init.vim \
 		-qO "${nvimCfg}" &> /dev/null
 fi
+echo -e "\nNeovim and init.vim both installed.\n"
 
 # Install the vim-plug plugin manager
+echo -e "\nDownloading vim-plug ...\n"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
 	--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo -e "\nVim-plug downloaded.\n"
 
 # Install Python3 in CentOS 
 # EPEL repo must be installed first
 if ! rpm -q epel-release &> /dev/null; then
 	yum install epel-release -y
 fi
+echo
+
 if [ ! -x "`command -v python3`" ]; then
 	yum install python36 -y
+	ln -s python36 /usr/bin/python3
 fi
-ln -s python36 /usr/bin/python3
+echo
 
 # Install pip3
 if [ ! -x "`command -v pip3`" ]; then
 	curl -O https://bootstrap.pypa.io/get-pip.py
 	python3 get-pip.py
 fi
+echo
 
 # Install the NeoVim Python module
 pip3 install --upgrade neovim
+echo
 
 # Print nvim's info
-nvim --version && echo -e '\n\nNeovim installed.'
+nvim --version
+echo -e "\nNeovim installed. Before the first startup, you MUST set $http_proxy!\n"
 
